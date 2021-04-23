4eb92aeb6a6685abbce79790add91b99ef5613b9
kimnanhee <kimnanhee0225@gmail.com>
2021-04-23 11:34:24 +0900

    [Add] 백준 11729번 하노이 탑 이동 순서

---
+++ 11729_하노이탑이동순서.c
@@ -0,0 +1,29 @@
+/*
+11729번 하노이 탑 이동 순서
+
+세 개의 장대가 있고 첫 번째 장대에는 반경이 서로 다른 n개의 원판이 쌓여 있다. 
+각 원판은 반경이 큰 순서대로 쌓여있다. 
+이제 수도승들이 다음 규칙에 따라 첫 번째 장대에서 세 번째 장대로 옮기려 한다.
+한 번에 한 개의 원판만을 다른 탑으로 옮길 수 있다.
+쌓아 놓은 원판은 항상 위의 것이 아래의 것보다 작아야 한다.
+이 작업을 수행하는데 필요한 이동 순서를 출력하는 프로그램을 작성하라. 단, 이동 횟수는 최소가 되어야 한다.
+*/
+#include <stdio.h>
+#include <math.h>
+void hanoi(int n, int from, int to, int via)
+{
+	if(n==1) printf("%d %d\n", from, to);
+	else {
+		hanoi(n-1, from, via, to);
+		printf("%d %d\n", from, to);
+		hanoi(n-1, via, to, from);
+	}
+}
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    printf("%d\n", (int)pow(2, n)-1);
+    hanoi(n, 1, 3, 2);
+    return 0;
+}
