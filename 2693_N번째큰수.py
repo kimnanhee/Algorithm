23fe83532d414f6b64fb017457369f00620e7a69
kimnanhee <kimnanhee0225@gmail.com>
2021-09-25 16:29:41 +0900

    [Modify] 2693번 N번째 큰 수

--- 2693_N번째큰수.py
+++ 2693_N번째큰수.py
@@ -1,3 +1,14 @@
-print(sorted(input().split())[7] for _ in range(int(input())))
+'''
+2693번 N번째 큰 수
 
-print(list(map(int, input().split())).sort()[7] for _ in range(int(input())))
+배열 A가 주어졌을 때, N번째 큰 값을 출력하는 프로그램을 작성하시오.
+배열 A의 크기는 항상 10이고, 자연수만 가지고 있다. N은 항상 3이다.
+
+첫째 줄에 테스트 케이스의 개수 T(1 ≤ T ≤ 1,000)가 주어진다.
+각 테스트 케이스는 한 줄로 이루어져 있고, 배열 A의 원소 10개가 공백으로 구분되어 주어진다.
+이 원소는 1보다 크거나 같고, 1,000보다 작거나 같은 자연수이다.
+
+각 테스트 케이스에 대해 한 줄에 하나씩 배열 A에서 3번째 큰 값을 출력한다.
+'''
+for _ in range(int(input())):
+    print(sorted(list(map(int, input().split())))[7])
