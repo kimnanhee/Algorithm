935d6080e107e584ac0cbd461baf3ecc8cb0bcf8
kimnanhee <kimnanhee0225@gmail.com>
2024-08-29 14:37:00 +0900

    2. Add Two Numbers

---
+++ Leet_2_addTwoNumbers.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        print(l1)
+        print(l2)
+
+s = Solution()
+
+res = s.addTwoNumbers([9,9,9,9,9,9,9], [9,9,9])
