6723d0233e4701e11186bc1c42cfa37611573649
kimnanhee <kimnanhee0225@gmail.com>
2024-08-31 23:35:32 +0900

    LeetCode 2.Add Two Numbers

--- Leet_2_addTwoNumbers.py
+++ Leet_2_addTwoNumbers.py
@@ -8,9 +8,28 @@ class ListNode:
 
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        print(l1)
-        print(l2)
+        res = ListNode(0)
+        tail = res
+        carry = 0
+
+        while l1 or l2 or carry:
+            digit1 = l1.val if l1 else 0
+            digit2 = l2.val if l2 else 0
+
+            vsum = digit1 + digit2 + carry
+
+            carry = vsum // 10
+            vsum = vsum % 10
+
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+
+            tail.next = ListNode(vsum)
+            tail = tail.next
+
+        return res.next
 
 s = Solution()
 
 res = s.addTwoNumbers([9,9,9,9,9,9,9], [9,9,9])
+print(res)
